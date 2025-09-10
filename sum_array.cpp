6d034bf9ec477500fc48d86d98f93c07817a3da0
me250003038-source <me250003038@iiti.ac.in>
2025-09-11 00:17:50 +0530

    Update sum_array.cpp

--- sum_array.cpp
+++ sum_array.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int sumArray(const vector<int>& arr) {
     // TODO: complete the function as per instructions
+    int sum=0;
+    for(int val: arr)
+    {
+           sum+=val;
+           }
+           return sum;
     
 }
 
@@ -16,4 +22,3 @@ int main() {
     cout << sumArray(arr) << "\n";
     return 0;
 }
-
